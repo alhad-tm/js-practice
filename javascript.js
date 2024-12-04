77f9d3fb10864d5171a989a6ddbb881c434f1674
alhad-tm <alhadchikkutm@gmail.com>
2024-12-04 18:44:54 +0530

    setTimeout

--- javascript.js
+++ javascript.js
@@ -14,14 +14,39 @@
 
 
 
-var i=0
+// var i=0
 
 
-var start=setInterval((()=>{
-    document.write("8")
-    i++
-    if (i>10)
-        clearInterval(start)
-}),400)
+// var starti=setInterval((()=>{
+//     document.write("8")
+//     i++
+//     if (i>10)
+//         clearInterval(starti)
+// }),400)
 
 
+
+// var j=0
+
+// var startj=setInterval((()=>{
+//     document.write("7")
+//     j++
+//     if (i>8)
+//         clearInterval(startj)
+// }),300)
+
+
+
+var k=8
+
+var kx= setInterval(()=>{
+    document.getElementById("counter").innerHTML= k ;
+    k--
+    if (k<=0)
+        clearInterval(kx)
+    
+},1000)
+
+setTimeout(()=>{
+ document.getElementById("star").innerHTML="stars"
+},3000)
